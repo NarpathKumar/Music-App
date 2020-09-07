e6905404bab9c70fffa4fd984337d6e66fec6329
NarpathKumar <53571476+NarpathKumar@users.noreply.github.com>
2020-09-07 12:26:46 +0530

    Update music.js

--- music.js
+++ music.js
@@ -1,5 +1,6 @@
 console.clear();
 
+
 $(function(){
     var randomArr = [];
     var musicMain = document.getElementById('music-div');
@@ -20,6 +21,11 @@ $.get("http://5dd1894f15bbc2001448d28e.mockapi.io/playlist", function(e){
     }
 });
 
+setInterval(function(){
+    var duration = Math.floor(audio.duration/60) + ": " +((audio.duration%60)/60).toFixed(2).split(".")[1];
+    $('#song-duration').text(duration);
+    $('#song-current-time').text(Math.floor(audio.currentTime))
+},0)
 function createList(id,url, name, artist, audios){
     var card = $('<div>').addClass('music-cards');
     card.id = id;
@@ -64,8 +70,7 @@ playPause.onclick=function(){
     }
 }
 
-$('#forward').click(function(){
-    var id = Number(musicMain.classList[0])+1;
+function playNextSong(id){
     $.get('http://5dd1894f15bbc2001448d28e.mockapi.io/playlist/'+id,function(e){
         $('#music-logo').attr({
             src: e.albumCover,
@@ -76,47 +81,53 @@ $('#forward').click(function(){
         audio.setAttribute('src',e.file);
         musicMain.className = e.id;
     })
+}
+$('#forward').click(function(){
+    var id = Number(musicMain.classList[0])+1;
+    if(id === 9){
+        id = 1
+        playNextSong(id)
+    }
+    playNextSong(id)
 })
 
 $('#backward').click(function(){
     var id = Number(musicMain.classList[0])-1;
     if(id === 0){
-        alert("Cant go backward")
+        playNextSong(randomArr[randomArr.length-1]);
     }
     else {
-        $.get('http://5dd1894f15bbc2001448d28e.mockapi.io/playlist/'+id,function(e){
-        $('#music-logo').attr({
-            src: e.albumCover,
-            alt: e.track
-        })
-        $('#song').text(e.track);
-        $('#artist').text(e.artist);
-        audio.setAttribute('src',e.file);
-        musicMain.className = e.id;
-    })
+        playNextSong(id);
     }
-    
 })
 
+var ranPrev = -1;
 audio.ontimeupdate=function(){
     $('#progress-report').css({
         "width" : (barWidth * audio.currentTime)/audio.duration
     }) 
     if(audio.currentTime === audio.duration){
-        var id = Number(musicMain.classList[0])+1;
-        $.get('http://5dd1894f15bbc2001448d28e.mockapi.io/playlist/'+id,function(e){
-        $('#music-logo').attr({
-            src: e.albumCover,
-            alt: e.track
-        })
-        $('#song').text(e.track);
-        $('#artist').text(e.artist);
-        audio.setAttribute('src',e.file);
-        musicMain.className = e.id;
-    })
+            if($('#shuffle').get(0).style.color === "blue"){
+                var random = randomArr[Math.floor(Math.random() * randomArr.length)];
+                if(random == ranPrev){
+                    playNextSong(random+1)
+                }
+                else {
+                    playNextSong(random)
+                    ranPrev = random
+                }
+            }else{
+                var id = Number(musicMain.classList[0])+1;
+                if(id > 8){
+                    id = 1
+                }
+                playNextSong(id)
+                $('#play-pause').get(0).className = "fas";
+                $('#play-pause').addClass("fa-pause-circle")
+            }
+       
     }
 }
-
 
 $('#progress-bar').click(function(e){
     var x = document.getElementById('progress-bar').getClientRects()[0].x;
@@ -129,6 +140,13 @@ $('#progress-bar').click(function(e){
     }
 })
 
+$("#volume").on({
+    mousedown : function(e){
+        console.log(e.clientX)
+    }
+})
+
+
 $('#restart').click(function(e){
     audio.currentTime = 0
 })
@@ -138,6 +156,7 @@ $('#shuffle').click(function(e){
         $("#shuffle").css({"color" : "blue"})
     }else $("#shuffle").css({"color" : "rgb(21, 18, 75)"})
 })
+
 
 })
 
